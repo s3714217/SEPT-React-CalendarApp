485a0f4a74339bf8dc8f21a1f84435207cec5ad1
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2019-09-17 11:21:05 -0700

    Add files via upload

--- frontend/todo-app/src/components/todo/TodoComponent.jsx
+++ frontend/todo-app/src/components/todo/TodoComponent.jsx
@@ -11,7 +11,9 @@ class TodoComponent extends Component {
         this.state = {
             id: this.props.match.params.id,
             description: '',
-            targetDate: moment(new Date()).format('YYYY-MM-DD')
+            targetDate: moment(new Date()).format('YYYY-MM-DD'),
+            title: ' ',
+            location: ' ',
         }
 
         this.onSubmit = this.onSubmit.bind(this)
@@ -36,11 +38,25 @@ class TodoComponent extends Component {
 
     validate(values) {
         let errors = {}
+        
+        if (!values.title) {
+            errors.title = 'Enter a Title'
+        } else if (values.title.length < 5) {
+            errors.title = 'Enter at least 3 Characters in Title'
+        }
+        
         if (!values.description) {
             errors.description = 'Enter a Description'
         } else if (values.description.length < 5) {
-            errors.description = 'Enter atleast 5 Characters in Description'
+            errors.description = 'Enter at least 5 Characters in Description'
         }
+        
+        if (!values.location) {
+            errors.location = 'Enter a location of the event'
+        } else if (values.location.length < 5) {
+            errors.location= 'Enter at least 5 Characters in Location'
+        }
+    
 
         if (!moment(values.targetDate).isValid()) {
             errors.targetDate = 'Enter a valid Target Date'
@@ -56,7 +72,9 @@ class TodoComponent extends Component {
         let todo = {
             id: this.state.id,
             description: values.description,
-            targetDate: values.targetDate
+            targetDate: values.targetDate,
+            title: values.title,
+            location: values.location,
         }
 
         if (this.state.id === -1) {
@@ -72,15 +90,15 @@ class TodoComponent extends Component {
 
     render() {
 
-        let { description, targetDate } = this.state
+        let { description, targetDate, location, title} = this.state
         //let targetDate = this.state.targetDate
 
         return (
             <div>
-                <h1>Todo</h1>
+                <h1>Add Event</h1>
                 <div className="container">
                     <Formik
-                        initialValues={{ description, targetDate }}
+                        initialValues={{ title, description, targetDate, location}}
                         onSubmit={this.onSubmit}
                         validateOnChange={false}
                         validateOnBlur={false}
@@ -94,15 +112,32 @@ class TodoComponent extends Component {
                                         className="alert alert-warning" />
                                     <ErrorMessage name="targetDate" component="div"
                                         className="alert alert-warning" />
+                                    <ErrorMessage name="location" component="div"
+                                        className="alert alert-warning" />
+                                    <ErrorMessage name="title" component="div"
+                                        className="alert alert-warning" />
+                                    
+                                    
+                                    <fieldset className="form-group">
+                                        <label>Title</label>
+                                        <Field className="form-control" type="text" name="title" />
+                                    </fieldset>
+                                    
                                     <fieldset className="form-group">
                                         <label>Description</label>
                                         <Field className="form-control" type="text" name="description" />
                                     </fieldset>
+                                    
                                     <fieldset className="form-group">
                                         <label>Target Date</label>
                                         <Field className="form-control" type="date" name="targetDate" />
                                     </fieldset>
-                                    <button className="btn btn-success" type="submit">Save</button>
+                                    
+                                    <fieldset className="form-group">
+                                        <label>Location</label>
+                                        <Field className="form-control" type="text" name="location" />
+                                    </fieldset>
+                                    <button className="btn btn-success" type="submit">Add</button>
                                 </Form>
                             )
                         }
