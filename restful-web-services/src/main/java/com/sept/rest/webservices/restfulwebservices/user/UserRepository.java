73d17907898c574cf95c772021a402545070649a
Tharvind <53845544+Tharvind@users.noreply.github.com>
2019-10-01 16:08:31 +0800

    update userrepository.java

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/user/UserRepository.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/user/UserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 	
-	Optional<User> findByUserID(long userID);
+	Optional<User> findByUserID(int userID);
 	
 
 }
