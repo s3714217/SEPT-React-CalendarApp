7709eece99fc0b47c0ab5863ac4df51dbd0667b3
s3690439 <41711318+s3690439@users.noreply.github.com>
2019-10-01 20:15:51 +1000

    Update EventJpaRepository.java
    Fixed methods and added new ones to the Repository

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/Event/EventJpaRepository.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/Event/EventJpaRepository.java
@@ -5,7 +5,15 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.sept.rest.webservices.restfulwebservices.Day.Day;
+
 @Repository
 public interface EventJpaRepository extends JpaRepository<Event, Long> {
+
+	void deleteById(int id);
+
+	Optional<Event> findEvent(int id);
+
+	Optional<Event> findEvent(Day day);
 
 }
