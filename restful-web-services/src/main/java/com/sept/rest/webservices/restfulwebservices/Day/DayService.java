de405372c456ca3ed4e07641c1f403f501662676
s3690439 <41711318+s3690439@users.noreply.github.com>
2019-10-01 20:13:52 +1000

    Update DayService.java
    fixing major bug fixes

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/Day/DayService.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/Day/DayService.java
@@ -2,10 +2,13 @@ package com.sept.rest.webservices.restfulwebservices.Day;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
 @Component
+@Service
 public class DayService {
     @Autowired
     DayJpaRepository repository;
