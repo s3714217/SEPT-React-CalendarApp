6f53e6dc5ae2f143b05e679c2f3c98f6d1581118
s3690439 <41711318+s3690439@users.noreply.github.com>
2019-10-04 11:35:44 +1000

    merging backend to final-merging

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/Event/EventController.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/Event/EventController.java
@@ -5,35 +5,58 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sept.rest.webservices.restfulwebservices.Day.Day;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/day/events")
 public class EventController {
     @Autowired
     EventService service;
 
-    @GetMapping
-    public List<Event> getEvents() {
-        return service.getEvents();
-    }
-
-    @PostMapping
-    public void postEvents(@RequestBody Event event) {
-        service.add(event);
-    }
-
-    @GetMapping("/{id}")
-    public Event getByNumAndMonth(@PathVariable(required = true) int id) {
-        return service.getEventById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete( @PathVariable(required = true) int id) {
-        service.delete(id);
-    }
+    @PostMapping(value = "/")
+	public void createEvent(@RequestBody Event event){
+		service.repository.addEvent(event);
+		
+		service.add(event);
+		
+		
+	}
+	
+	@DeleteMapping(value = "/")
+	public void DeleteEvent(@RequestBody Event event){
+		Event event1 = service.repository.findEvent(event.getEventID());
+		//FIT HERE EVENT SETUP
+		service.delete(event1.getEventID());
+		
+		
+	}
+	
+	
+	
+	@PutMapping(value = "/")
+	public void UpdateEvent(@RequestBody Event event){
+		Event event1 = service.repository.findEvent(event.getEventID());
+		//FIT HERE EVENT SETUP
+		service.update(event1.getEventID(), event1);
+		
+	}
+	
+	@SuppressWarnings("null")
+	@RequestMapping(value = "/")
+	public List<Event> DisplayEvents(){
+		
+	
+		List<Event> events = service.repository.findAll();
+		
+		return events;
+		
+	}
+    
 }
